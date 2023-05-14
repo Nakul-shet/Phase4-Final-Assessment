d111a8dfaf7716681916a9182249316c15dfbb95
Nakul N Shet <nakulnshet1@gmail.com>
2023-05-14 20:42:01 +0530

    Add project files.

---
+++ PizzaPortalTests/BaseTest.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PizzaPortalTests
+{
+    internal class BaseTest
+    {
+        IWebDriver driver;
+
+        protected IWebDriver GetDriver()
+        {
+            return driver;
+        }
+        [OneTimeSetUp]
+
+        public void OneTimeSetup()
+        {
+            TestContext.Progress.WriteLine("One Time Setup Invoked.");
+        }
+        [SetUp]
+
+        public void Setup()
+        {
+            driver = CreateDriver("chrome");
+            /*driver.Manage().Window.Size = new Size(1134, 824);*/
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:5182/");
+           
+        }
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [OneTimeTearDown]
+
+        public void OneTimeTearDown()
+        {
+            TestContext.Progress.WriteLine("One Time Tear Down Invoked.");
+        }
+
+        private IWebDriver CreateDriver(string browserName)
+        {
+            switch (browserName.ToLowerInvariant())
+            {
+                case "chrome":
+                    return new ChromeDriver();
+
+                case "firefox":
+                    return new FirefoxDriver();
+
+                case "edge":
+                    return new EdgeDriver();
+                default:
+                    throw new Exception("Provided browser is not supported.");
+            }
+        }
+
+
+    }
+}
---
+++ PizzaPortalTests/BasicElementOrganizedTests.cs
@@ -0,0 +1,78 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPortalTests
+{
+    
+    internal class BaseElementOrganizedTests : BaseTest
+    {
+        [Test]
+        public void SimpleTest()
+        {
+
+            GetDriver().FindElement(By.Id("privacy")).Click();
+            GetDriver().FindElement(By.Id("register")).Click();
+            GetDriver().FindElement(By.Id("Name")).SendKeys("Nakul");
+            GetDriver().FindElement(By.Id("UserName")).SendKeys("nakulshet");
+            GetDriver().FindElement(By.Id("password")).SendKeys("N@kuL");
+            GetDriver().FindElement(By.Id("Email")).SendKeys("nakulshet1@gmail.com");
+            GetDriver().FindElement(By.Id("register-btn")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("UserName")).SendKeys("nakulshet");
+            GetDriver().FindElement(By.Id("password")).SendKeys("N@kuL");
+            GetDriver().FindElement(By.Id("loginbtn")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("avail-btn")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("view-btn")).Click();
+            Thread.Sleep(3000);
+            GetDriver().FindElement(By.Id("cart-btn")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("check-btn")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("UserName")).SendKeys("Nakul");
+            GetDriver().FindElement(By.Id("Email")).SendKeys("nakulnshet1@gmail.com");
+            GetDriver().FindElement(By.Id("Phone")).SendKeys("8050558156");
+            GetDriver().FindElement(By.Id("Addre")).SendKeys("Mangalore");
+            Thread.Sleep(2000);
+            var countryDropDown = GetDriver().FindElement(By.Id("country"));
+            var countrySelectElement = new SelectElement(countryDropDown);
+            countrySelectElement.SelectByText("India");
+            Thread.Sleep(2000);
+            var stateDropDown = GetDriver().FindElement(By.Id("state"));
+            var stateSelectElement = new SelectElement(stateDropDown);
+            stateSelectElement.SelectByText("Andhra Pradesh");
+            GetDriver().FindElement(By.Id("Zip")).SendKeys("520007");
+            /*GetDriver().FindElement(By.Id("same-address")).Click();
+            GetDriver().FindElement(By.Id("save-info")).Click();*/
+            Thread.Sleep(2000);
+            var checkbox1 = GetDriver().FindElement(By.Id("same-address"));
+            checkbox1.Click();
+            var checkbox2 = GetDriver().FindElement(By.Id("save-info"));
+            checkbox2.Click();
+            GetDriver().FindElement(By.Id("checkout-btn")).Click();
+            Thread.Sleep(2000);
+            var paymentDropDown = GetDriver().FindElement(By.Id("select"));
+            var paymentSelectElement = new SelectElement(paymentDropDown);
+            paymentSelectElement.SelectByText("Credit Card");
+            GetDriver().FindElement(By.Id("payment")).Click();
+            Thread.Sleep(2000);
+            GetDriver().FindElement(By.Id("logout-btn")).Click();
+            Thread.Sleep(2000);
+
+
+
+
+        }
+
+       
+
+      
+    }
+}
